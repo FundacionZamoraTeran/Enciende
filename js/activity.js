cd22eb56c72b15ca3b5d161d13ae78c80ea1daee
Yader Velásquez <yajosev@gmail.com>
2015-10-29 10:16:09 -0600

    Nivel 1

--- js/activity.js
+++ js/activity.js
@@ -19,6 +19,24 @@ define(function (require) {
         }, interval);
     }
 
+    function win_msg() {
+        $('#win').removeClass('hidden');
+    }
+
+    function fail_msg() {
+        $('#bad').removeClass('hidden');
+        setTimeout(function() {
+            $('#bad').addClass('hidden');
+        }, 1500);
+    }
+
+    function good_msg() {
+        $('#good').removeClass('hidden');
+        setTimeout(function() {
+            $('#good').addClass('hidden');
+        }, 1500);
+    }
+
     function off(n) {
         setTimeout(function() {
             $('button[value="' + n + '"]').removeClass('active');
@@ -35,11 +53,15 @@ define(function (require) {
     function MemoryGame() {
         this.numbers = [];
         this.count = 0;
+        this.win_count = 0;
 
     };
 
     MemoryGame.prototype.randomNumber = function() {
         var n = Math.floor(Math.random() * 10);
+        while (this.numbers[this.numbers.length - 1] === n) {
+            n = Math.floor(Math.random() * 10);
+        }
         this.numbers.push(n);
         return this.numbers;
     };
@@ -48,20 +70,42 @@ define(function (require) {
         var n = parseInt(number);
         if (n === this.numbers[this.count]) {
             this.count++;
+            // Good
             if (this.count >= this.numbers.length) {
                 this.count = 0;
+                this.win_count = 0;
                 return true;
             }
-
+            // Bien, pero aun no has ganado todas
             else {
+                this.win_count++;
                 return false;
             }
         }
+
+        // Muy mal
         else {
             this.count = 0;
+            fail_msg();
             return false;
         }
     };
+
+    MemoryGame.prototype.win = function() {
+        if (this.win_count >= 2) {
+            return true;
+        }
+
+        else {
+            return false;
+        }
+    }
+
+    MemoryGame.prototype.reset = function() {
+        this.numbers = [];
+        this.count = 0;
+        this.win_count = 0;
+    }
 
     // Manipulate the DOM only when it is ready.
     require(['domReady!'], function (doc) {
@@ -87,10 +131,21 @@ define(function (require) {
                 activate(numbers);
                 $('.button-number').on('click', function(e) {
                     e.preventDefault();
-                    if(game.inputNumber($(this).attr('value'))) {
+                    if(game.win()) {
+                        game.reset();
+                        win_msg();
+                    }
+                    else if(game.inputNumber($(this).attr('value'))) {
                         numbers = game.randomNumber();
                         activate(numbers);
+                        good_msg();
                     }
+                });
+                $('#play-again').on('click', function(e) {
+                    e.preventDefault();
+                    $('#win').addClass('hidden');
+                    var numbers = game.randomNumber();
+                    activate(numbers);
                 });
             }
 
